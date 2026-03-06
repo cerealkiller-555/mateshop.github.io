a07d51ec3db9ad84f848833968dbc2d816093a98
Malak Hany <151863282+cerealkiller-555@users.noreply.github.com>
2026-03-06 03:34:05 +0200

    Fix cart-icon alignment to flex-end

--- script.js
+++ script.js
@@ -1,4 +1,46 @@
-function open_form() {
-    alert("Form opened!");
+function openForm(product) {
+    document.getElementById("modal").classList.add("show");
+    document.getElementById("overlay").classList.add("show");
+    document.getElementById("productName").innerText = "Product: " + product;
+
+}
+
+function closeForm() {
+    document.getElementById("modal").classList.remove("show");
+    document.getElementById("overlay").classList.remove("show");
+}
+
+function submitOrder(e) {
+    e.preventDefault();
+    alert("Order submitted successfully!");
+    closeForm()
+}
+
+
+let cart = [
+
+]
+
+function addToCart() {
+    document.getElementById("cart").classList.add("show");
+    document.getElementById("overlay").classList.add("show");
+    // Add product to cart array
+    cart.push(product);
+    // Update cart display
+    cart.display();
+}
+
+function removeFormCart() {
+    document.getElementById("cart").classList.remove("show");
+    document.getElementById("overlay").classList.remove("show");
+    // Remove product from cart array
+
+}
+
+function updateQuantity() {
+
+}
+
+function renderCart() {
 
 }
